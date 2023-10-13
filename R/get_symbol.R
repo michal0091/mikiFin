2d3543274de520726052b71cdc028a2125214bde
Michal Kinel <michal.kinel@gmail.com>
2023-10-14 00:08:51 +0200

    Add time_interval aux function

--- R/get_symbol.R
+++ R/get_symbol.R
@@ -1,2 +1,10 @@
 # get_symbols -------------------------------------------------------------
 
+time_interval <- function(interval) {
+  data("time_intervals", envir = environment())
+  stopifnot(interval %in% names(time_intervals))
+
+  time_intervals[[interval]]
+}
+
+
